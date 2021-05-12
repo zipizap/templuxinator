fe449dadfa086ed5907209c9832484e2465d0e5f
zipizap <zipizap123@gmail.com>
2021-05-13 01:58:00 +0200

    update

--- github_new_gorelease.sh
+++ github_new_gorelease.sh
@@ -21,7 +21,8 @@ cd "${__dir}"
 # Check GITHUB_TOKEN env-var
 (set +x; GITHUB_TOKEN="${GITHUB_TOKEN?missing env-var}")
 
-# Create tag
+## Create tag
+# NOTE: undo tag with:    git tag -d v1.0.0 && git push --delete origin v1.0.0
 git tag -a v1.0.0 -m "First release"
 git push origin v1.0.0
 
